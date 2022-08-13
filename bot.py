35202bb5a577d756685cac8f2e7b765dcd32af08
hanbee95 <nospicynocilantro@gmail.com>
2022-08-13 13:10:17 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,113 @@
+#reference: https://hextechdocs.dev/how-to-verify-your-application-using-github/
+
+import discord
+import os
+import requests
+from dotenv import load_dotenv
+from discord.ext import commands
+from datetime import date
+import json
+load_dotenv()
+
+#get discord token and riot api key from .evn file
+TOKEN = os.environ['DISCORD_TOKEN']
+APIKEY = os.environ['APIKEY']
+
+#each server should have different reported db
+class server:
+    def __init__(self):
+        self.reported = []
+        self.reportedid = []
+        self.report = {} #{reportedid:[reporter,date,reason]}
+
+servers = {}
+bot = commands.Bot(command_prefix='&', description='db for discord server')
+@bot.event
+async def on_ready():
+    GUILD = []
+    async for guild in bot.fetch_guilds(limit=150):
+        servers[guild.name] = server()
+        GUILD.append(guild.name)    
+    #which guild use this discord bot : now only HAN server
+    print(f"Allowed servers: {GUILD}")
+
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('-'*20)
+    await bot.change_presence(activity=discord.Game(name='&help'))
+
+@bot.command(name='test', aliases=['t','Test','T'], help='For test')
+async def test(ctx):
+    print ("check")
+    await ctx.send("test!!!")
+
+#report user to DB
+@bot.command(name='report', aliases=['r','Report','R'], help='Reporting league user')
+async def report(ctx, *reporting):
+    #  &report Kevin Kookies, 분당 와드 한개
+    reportingline = ' '.join(reporting)
+    await ctx.send(reportingline)
+
+#search use in DB
+@bot.command(name='search', aliases=['s','Search','S'], help='Searching users in the dict')
+async def search(ctx, *searching):
+    searchingline = ' '.join(searching)
+    await ctx.send(searchingline)
+  
+#just for fun. mimicing multisearch. very slow
+@bot.command(name='match', aliases=['m','Match','M'], help='Match history for searched user')
+async def match(ctx, *searching):
+    searchingline = ' '.join(searching)
+    print(searchingline)
+    joincnt = searchingline.count('joined the lobby')
+    joinppl = []
+    oneppl = ''
+    for oneterm in searching:
+        if oneterm == 'joined':
+            joinppl.append(oneppl)
+            oneppl = ''
+        elif oneterm == 'lobby':
+            oneppl = ''
+        else:
+            oneppl = oneppl + oneterm
+    multisearch = {}
+    for idx, person in enumerate(joinppl):
+        summonerInfo = (requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + person + '?api_key=' + APIKEY)).json()
+        if len(summonerInfo)<=1:
+            print('There is no such username ' + person)
+            await ctx.send('There is no such username ' + person)
+        else:
+            puuid = summonerInfo['puuid']
+            matchhistory = (requests.get('https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?type=ranked&start=0&count=5' + '&api_key=' + APIKEY)).json()
+            #print(matchhistory)
+            for match in matchhistory:
+                prev = (requests.get('https://americas.api.riotgames.com/lol/match/v5/matches/'+match+'?api_key=' + APIKEY)).json()
+                participantinfo = prev['info']['participants']
+                for eachparti in participantinfo:
+                    if eachparti['puuid'] == puuid:
+                        if eachparti['win'] == 1:
+                            win = 'win'
+                        else:
+                            win = 'lose'
+                        if eachparti['summonerName'] in multisearch.keys():
+                            multisearch[eachparti['summonerName']].append([win, eachparti['championName'], eachparti['kills'], eachparti['deaths'], eachparti['assists']])
+                        else:
+                            multisearch[eachparti['summonerName']] = [[win, eachparti['championName'], eachparti['kills'], eachparti['deaths'], eachparti['assists']]]
+                        
+                        # print(eachparti['summonerName'])
+                        # print(eachparti['win'])
+                        # print(eachparti['championName'])
+                        # print(eachparti['killingSprees'])
+                        # print(eachparti['kills'])
+                        # print(eachparti['deaths'])
+                        # print(eachparti['assists'])
+                        # print(eachparti['basicPings'])
+                        # print(eachparti['challenges']['enemyJungleMonsterKills'])
+                        # print(eachparti['challenges']['soloKills'])
+                        # print(eachparti['challenges']['alliedJungleMonsterKills'])
+    print(json.dumps(multisearch,sort_keys=True, indent=4))
+    await ctx.send(multisearch)
+
+
+bot.run(TOKEN)
